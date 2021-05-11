b5489c24ef83a3792ed4aa59e38786e836bbbf95
Michael Oben <michael.oben@gmail.com>
2021-05-11 17:39:22 -0400

    finished logic

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -22,69 +22,125 @@ namespace ConsoleApp1
             Console.WriteLine("--------------------------------------------------------------------------------------------------");
             Console.WriteLine("--------------------------------------------------------------------------------------------------");
             Console.WriteLine("");
-            Console.WriteLine(name + ", you have " + totalDays + " days to make the most money possible via buying and selling bitcoins.");
+            Console.WriteLine(name + ", you have " + totalDays + " days to make the most money possible via trading bitcoins.");
             Console.WriteLine("");
-            Console.WriteLine("");
-
-            Console.WriteLine("Currently you have " + totalBitcoin + " bitcoins. And you have $" + totalMoney + ".");
+            Console.WriteLine("When there are no more days left, your final score will equal the following: [total money + bitcoin assets]");
             Console.WriteLine("");
 
+            Console.WriteLine("You will start out with " + totalBitcoin + " bitcoins.");
+            Console.WriteLine("");
 
-            for (int currentDay = 0; currentDay <= totalDays; currentDay++)
+
+            for (int currentDay = 1; currentDay <= totalDays; currentDay++)
             {
                 // local variables
                 int daysLeft = totalDays - currentDay;
                 int btcPrice = generateRandom.Next(0, 70000); // daily bitcoin price
+                string input = "";
 
-                // print current day, days left
-                Console.WriteLine("Today is day " + currentDay + ". You have " + daysLeft + " days left.");
-                Console.WriteLine("");
-                Console.WriteLine("");
+                
                 
                 while (input != "done")
                 {
                     // prompt - menu
                     Console.WriteLine("-------------------------------------------------------------- ");
-                    Console.WriteLine("What do you want to do? Type MENU to see the list of options.");
+                    Console.WriteLine("Today is day " + currentDay + ". You have " + daysLeft + " days left.");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                    Console.WriteLine("Current bitcoin price is: $" + btcPrice);
+                    Console.WriteLine("You have " + totalBitcoin + " bitcoins.");
+                    Console.WriteLine("You have $" + totalMoney + " dollars.");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                    Console.WriteLine("What do you want to do?");
+                    Console.WriteLine("   // type 'buy' to buy bitcoins");
+                    Console.WriteLine("   // type 'sell' to sell your bitcoins");
+                    Console.WriteLine("   // type 'done' if you are done trading for the day");
+                    Console.WriteLine("   // user actions are Case Sensitive");
                     Console.WriteLine("-------------------------------------------------------------- ");
                     
                     // prompt - input
-                    string input = Console.ReadLine();
+                    input = Console.ReadLine();
 
-                    // logic, use else if
-                    if (input == "menu")
+                    // sell logic
+                    if (input == "sell")
                     {
-                        Console.WriteLine();
-                        Console.WriteLine("-------------------------------------------------------- ");
-                        Console.WriteLine("// type 'price' to see today's btc price");
-                        Console.WriteLine("// type 'money' to see how much money you have ");
-                        Console.WriteLine("// type 'buy' to buy bitcoins");
-                        Console.WriteLine("// type 'sell' to sell your bitcoins");
-                        Console.WriteLine("-------------------------------------------------------- ");
-                        Console.WriteLine();
-                        break;
+                        Console.Clear();
+                        Console.WriteLine("You are about to sell " + totalBitcoin + " bitcoins for the following price: $" + btcPrice);
+                        Console.WriteLine("");
+                        Console.WriteLine("Do you want to proceed? Type Y or N");
+                        Console.WriteLine("");
+                        string sellBitcoin = Console.ReadLine();
+
+                        if (sellBitcoin == "Y")
+                        {
+                            Console.Clear();
+                            totalMoney = totalBitcoin * btcPrice;
+                            totalBitcoin = 0;
+                            Console.WriteLine("You sold your bitcoins. You now have $" + totalMoney);
+                        }
+
+                        // FIXME
+                        // need to add logic for if you sell with 0 bitcoins. that would wipe out your total money
+
+                        else
+                        {
+                            Console.WriteLine("Sending you back");
+                        }
+
                     }
 
-                    else if (input == "price")
+                    // buy logic
+                    if (input == "buy")
                     {
-                        // print btc price
-                        Console.WriteLine("Current bitcoin price is: $" + btcPrice);
-                        break;
+                        int potentialBuyAmount = (totalMoney / btcPrice);
+
+                        if (potentialBuyAmount > 0)
+                        {
+                            // let them buy
+                            Console.Clear();
+                            Console.WriteLine("You are about to buy " + potentialBuyAmount + " bitcoins for the following price: $" + btcPrice);
+                            Console.WriteLine("");
+                            Console.WriteLine("Do you want to proceed? Type Y or N");
+                            Console.WriteLine("");
+                            string buyBitcoin = Console.ReadLine();
+
+                            if (buyBitcoin == "Y")
+                            {
+                                Console.Clear();
+                                totalBitcoin = potentialBuyAmount;
+                                totalMoney = 0;
+                                Console.WriteLine("You bought some bitcoins. You now have " + totalBitcoin + " bitcoins.");
+                            }
+
+                            // FIXME
+                            // need to add when you can't buy
+
+                            else
+                            {
+                                Console.WriteLine("Sending you back");
+                            }
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Sending you back");
+                        }
+
+
                     }
 
-                   
-                    }
-
-                    else if (input == "done")
+                    if (input == "done")
                     {
                         // exit while loop
+                        Console.Clear();
                         break;
                     }
 
                     else
                     {
                         // 'don't understand, please try again'
-                        Console.WriteLine("don't understand, try again");
+                        Console.WriteLine("");
                     }
 
                 }
